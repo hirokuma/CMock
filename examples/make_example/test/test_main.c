53c4bd2c14689d333312b3eb4f019395425caca5
hirokuma <hiro9ma@gmail.com>
2024-09-12 18:06:16 +0900

    check NG: return value and arguments

--- examples/make_example/test/test_main.c
+++ examples/make_example/test/test_main.c
@@ -1,21 +1,24 @@
 #include "unity.h"
 #include "mock_foo.h"
 
+int foo_main(int a, int b);
+
 void setUp(void)
 {
 }
 
 void tearDown(void)
 {
+    // mock_foo_Verify();
 }
 
 void test_main_should_initialize_foo(void)
 {
-    TEST_IGNORE_MESSAGE("TODO: Implement main!");
+    // TEST_IGNORE_MESSAGE("TODO: Implement main!");
 }
 
 void test_main_foo(void)
 {
-    foo_ExpectAndReturn(1, 2, 3);
+    foo_ExpectAndReturn(2, 2, 3);
     TEST_ASSERT_EQUAL_INT(6, foo_main(1, 2));
 }
